b24db4f7fe5fa4e366c65fd05a1630c0073d2625
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-13 10:12:27 +0530

    connectionSet

--- unit-1/evaluation/NodeJs_Unit2/Day5/index.js
+++ unit-1/evaluation/NodeJs_Unit2/Day5/index.js
@@ -1,1 +1,23 @@
 // all dependencies are added to the folder in this ok guys
+const express = require("express");
+const app = express();
+const { connection } = require("./db");
+const { userRouter } = require("./Routes/UserRoutes");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to HomePage`);
+});
+
+app.use("/users", userRouter);
+
+app.listen(8000, async () => {
+  try {
+    await connection;
+    console.log("Connected to Atlas Server...");
+  } catch (err) {
+    console.log(err.message);
+  }
+  console.log("Server is running...");
+});
