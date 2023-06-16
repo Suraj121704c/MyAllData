cc14e7c5eb77aa74aac86cdb96869c07f0db8d76
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-16 09:37:08 +0530

    modelAdded

---
+++ unit-1/evaluation/NodeJs_Unit2/Day5_blacklistBlogginApp/backend/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connection = mongoose.connect(
+  "mongodb+srv://suraj121704c:singh123@cluster0.ndbapyz.mongodb.net/day5_unit2_Assignment?retryWrites=true&w=majority"
+);
+
+module.exports = {
+  connection,
+};
--- unit-1/evaluation/NodeJs_Unit2/Day5_blacklistBlogginApp/backend/index.js
+++ unit-1/evaluation/NodeJs_Unit2/Day5_blacklistBlogginApp/backend/index.js
@@ -1,1 +1,20 @@
-abhi updpapte nhi hua hao 
+const express = require("express");
+const { connection } = require("./connection");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to HomePage!!!");
+});
+
+app.listen(5000, async () => {
+  try {
+    await connection;
+    console.log("Connected to Atlas Server...");
+  } catch (error) {
+    console.log(error.message);
+  }
+  console.log("Server is running...");
+});
