f7135b9fd35d96c13039a3054853cd023d50ccfd
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-16 11:20:17 +0530

    bloggingbackendDOne

--- unit-1/evaluation/NodeJs_Unit2/Day5_blacklistBlogginApp/backend/index.js
+++ unit-1/evaluation/NodeJs_Unit2/Day5_blacklistBlogginApp/backend/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const { connection } = require("./connection");
 const { userRouter } = require("./Routes/UserRoutes");
 const { BlogRouter } = require("./Routes/BlogRoutes");
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 
