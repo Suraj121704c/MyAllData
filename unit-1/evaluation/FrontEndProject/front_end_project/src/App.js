f73e03dc15369f232af9ac7a29d37c57a86bb994
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-06-27 08:56:50 +0530

    sidebar Done

--- unit-1/evaluation/FrontEndProject/front_end_project/src/App.js
+++ unit-1/evaluation/FrontEndProject/front_end_project/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Left from './Components/Left';
+import { RIght } from './Components/RIght';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="App" style={{width : "90%" , height : "90%" , margin : "50px auto" , display : "flex" , gap : "10"}}>
+       <Left />
+       <RIght />
     </div>
   );
 }
---
+++ unit-1/evaluation/FrontEndProject/front_end_project/src/Components/Left.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Left = () => {
+  return <div style={{width : "25%" , border : "1px solid black"}}>
+    
+  </div>;
+};
+
+export default Left;
