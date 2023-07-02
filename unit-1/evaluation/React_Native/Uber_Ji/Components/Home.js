26c64578a7a41d2b54b4b67d23f8922bbfa34407
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-07-02 15:55:44 +0530

    deploy

--- unit-1/evaluation/React_Native/Uber_Ji/Components/Home.js
+++ unit-1/evaluation/React_Native/Uber_Ji/Components/Home.js
@@ -5,13 +5,25 @@ import axios from "axios";
 const HomeScreen = ({ navigation }) => {
   const [items, setItems] = useState([]);
 
- 
+  function server() {
+    axios.get('https://backend-mock-5.onrender.com/products')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+    });
+  }
+
+  useEffect(() => {
+    server();
+  }, []);
 
   return (
     <View>
       <Button title="Add Item" onPress={() => navigation.navigate("Add")} />
-      
-      
+      <Text>Aur Nunu</Text>
+      <Text>Aur Nunu</Text>
     </View>
   );
 };
