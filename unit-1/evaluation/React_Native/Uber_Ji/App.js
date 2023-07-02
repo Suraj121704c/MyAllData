d5345b2dad273b7aef1df7316ec9c165385c2073
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-07-02 07:46:32 +0530

    euber

--- unit-1/evaluation/React_Native/Uber_Ji/App.js
+++ unit-1/evaluation/React_Native/Uber_Ji/App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
     </View>
   );
 }
