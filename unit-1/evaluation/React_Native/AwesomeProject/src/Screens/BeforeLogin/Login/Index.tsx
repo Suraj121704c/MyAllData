302fe99aa729a4c992940d4c3bb98bf48b669247
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-07-04 16:28:04 +0530

    restartTime

--- unit-1/evaluation/React_Native/AwesomeProject/src/Screens/BeforeLogin/Login/Index.tsx
+++ unit-1/evaluation/React_Native/AwesomeProject/src/Screens/BeforeLogin/Login/Index.tsx
@@ -1,4 +1,4 @@
-import React ,  { useState } from 'react';
+import React, {useState} from 'react';
 import {
   Image,
   ImageBackground,
@@ -6,30 +6,34 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-  Dimensions ,
-  ScrollView 
+  Dimensions,
+  ScrollView,
 } from 'react-native';
 
 // user-defining import
 import {styles} from './Styles';
 import {apple, facebook, google} from '../../../Assests/Utils/images';
 
+
 const Login = () => {
   const [dimensions, setDimensions] = useState(Dimensions.get('window'));
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLayoutChange = () => {
     setDimensions(Dimensions.get('window'));
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
-    <View style={styles.BigBox} onLayout={handleLayoutChange}>   
-    <ScrollView style={styles.ScrollJi}>
- <ImageBackground
+    <View style={styles.BigBox} onLayout={handleLayoutChange}>
+      <ScrollView style={styles.ScrollJi}>
+        <ImageBackground
           source={{
             uri: 'https://assets.gqindia.com/photos/5cdc07a454004370583c3a84/1:1/w_1080,h_1080,c_limit/top-image94.jpg',
-          }} 
-          >
+          }}>
           <View>
             <Text style={styles.HeadingTop}>
               B<Text style={styles.HeadingSpan}>EER STORE</Text>
@@ -42,49 +46,57 @@ const Login = () => {
             </View>
           </View>
         </ImageBackground>
-      <View style={styles.CurverBox}>
-        <View style={styles.EmailBoxFirst}>
-          <TextInput
-            placeholder="    Email Address"
-            style={styles.EmailAdress}
-          />
-          <TextInput placeholder="    Password" style={styles.Password} />
-          <TextInput style={styles.ForgetText}>Forget Password ?</TextInput>
-          <View style={styles.LoginBox}>
-            <TouchableOpacity style={styles.Button}>
-              <Text style={styles.ButtonText}>LOGIN</Text>
-            </TouchableOpacity>
+        <View style={styles.CurverBox}>
+          <View style={styles.EmailBoxFirst}>
+            <TextInput placeholder="Email Address" style={styles.EmailAdress} />
+
+           
+              <TextInput
+                placeholder="Password"
+                style={styles.Password}
+                secureTextEntry={!showPassword}
+              />
+             
+         
+
+            <TextInput style={styles.ForgetText}>Forget Password ?</TextInput>
+            <View style={styles.LoginBox}>
+              <TouchableOpacity style={styles.Button}>
+                <Text style={styles.ButtonText}>LOGIN</Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.orLoginWith}>or Login With</Text>
+            <View style={styles.ButtonBox}>
+              <TouchableOpacity style={styles.GoogleButton}>
+                <Image source={google} style={styles.GoogleImage} />
+                <Text style={styles.GoogleButtonText}>
+                  Continue with Google
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.ButtonBox}>
+              <TouchableOpacity style={styles.FacebookButton}>
+                <Image source={facebook} style={styles.GoogleImage} />
+                <Text style={styles.FacebookButtonText}>
+                  Continue with Facebook
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.ButtonBox}>
+              <TouchableOpacity style={styles.AppleButton}>
+                <Image source={apple} style={styles.GoogleImage} />
+                <Text style={styles.FacebookButtonText}>
+                  Continue with Apple
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.NewToBeerStore}>
+              New to The Beer Store?
+              <Text style={styles.CreateAnAccount}> Create an account.</Text>
+            </Text>
           </View>
-          <Text style={styles.orLoginWith}>
-            or Login With
-          </Text>
-          <View style={styles.ButtonBox}>
-            <TouchableOpacity style={styles.GoogleButton}>
-              <Image source={google} style={styles.GoogleImage} />
-              <Text style={styles.GoogleButtonText}>Continue with Google</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.ButtonBox}>
-            <TouchableOpacity style={styles.FacebookButton}>
-              <Image source={facebook} style={styles.GoogleImage} />
-              <Text style={styles.FacebookButtonText}>
-                Continue with Facebook
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.ButtonBox}>
-            <TouchableOpacity style={styles.AppleButton}>
-              <Image source={apple} style={styles.GoogleImage} />
-              <Text style={styles.FacebookButtonText}>Continue with Apple</Text>
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.NewToBeerStore}>
-            New to The Beer Store?
-            <Text style={styles.CreateAnAccount}> Create an account.</Text>
-          </Text>
         </View>
-      </View>
-    </ScrollView>  
+      </ScrollView>
     </View>
   );
 };
